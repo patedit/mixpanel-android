da21063ae21b6e2a70ea84bd75c2326e7d948364
Sergio Alonso <sergio.alonso@mixpanel.com>
2017-02-15 19:15:34 -0800

    wip

--- release.sh
+++ release.sh
@@ -162,7 +162,7 @@ git add .
 git commit -m "Update documentation for $releaseVersion"
 git push origin gh-pages
 
-printf "\n${GREEN}All done!\n"
+printf "\n${GREEN}All done! ¯\_(ツ)_/¯ \n"
 printf "Make sure you make a new release at https://github.com/mixpanel/mixpanel-android/releases/new\n"
 printf "Also, do not forget to update our CHANGELOG (https://github.com/mixpanel/mixpanel-android/wiki/Changelog)\n"
 printf "And finally, release the library from https://oss.sonatype.org/index.html\n\n${NC}"
